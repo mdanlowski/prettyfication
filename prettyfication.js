5a71f7349158faae7ff038c0c001f05aa687b699
mdanlowski <marcindanlowski@gmail.com>
2019-01-31 18:04:47 +0100

    stacking feature development attempts

--- prettyfication.js
+++ prettyfication.js
@@ -1,27 +1,74 @@
 //--------prettyfication-setup--------
 // @TODO each alert div to be created dynamically by the script - for development the element exists in HTML
 // @TODO if more than 1 alert comes at a time they should stack - margin set in js
-var pAlertBody;
+var pAlertBody = "<div class='prettyfication ptfn-hidden'></div>";
+var pAlertCount = 0;
 
 $(function(){
 
-	pAlertBody =  $('.prettyfication');
+	// pAlertBody =  $('.prettyfication');
 
-	pAlertBody.click(function(){
-		$(this).removeClass('ptfn-show').addClass('ptfn-hidden');//.fadeIn();
-	});
+	// pAlertBody.click(function(){
+	// 	$(this).removeClass('ptfn-show').addClass('ptfn-hidden');//.fadeIn();
+	// });
+
 
 });
 
 
 // pAlert is a Pretty Alert - prettyfication main feature
-function pAlert(message, alertType) {
-	pAlertBody.removeClass().addClass('prettyfication ptfn-hidden');
-	pAlertBody.text(message);
-	pAlertBody.addClass('ptfn-show').addClass(alertType);//.fadeOut();
+var pAlert = function(message, alertType) {
+	if(pAlertCount < 0) pAlertCount = 0;
+
+	$('#prettyfication-box').append(pAlertBody);
+	var pAlertInstance = $('#prettyfication-box div:last-child');
+	// console.log(pAlertInstance);
+
+	var instanceOffset = (40+pAlertCount*80).toString() + 'px';
+	pAlertInstance[0].style.marginTop = instanceOffset;
+
+	pAlertCount++;
+
+	pAlertInstance.removeClass().addClass('prettyfication ptfn-hidden');
+	pAlertInstance.text(message);
+	pAlertInstance.removeClass('ptfn-hidden').addClass('ptfn-show').addClass(alertType);//.fadeOut();
+
+	pAlertInstance.click(function(){
+		console.log($(this));
+		// console.log($(this)[0]);
+
+		$(this).removeClass('ptfn-show').addClass('ptfn-hidden');//.fadeIn();
+
+		var this_ = $(this)[0];
+
+		setTimeout(function() {
+			// console.log(this_.style.marginTop);
+			$('.prettyfication').each(function(i,e){ $(this).css("margin-top", parseInt($(this)[0].style.marginTop)-80) });
+		}, 500);
+
+		removeInstance($(this));
+	});
 
 	setTimeout(function() {
-		pAlertBody.removeClass('ptfn-show').addClass('prettyfication ptfn-hidden');
-	}, 3000);
+		pAlertInstance.removeClass('ptfn-show').addClass('ptfn-hidden');
+		removeInstance($(this));
+
+	}, 4000);
+
+
+	function removeInstance(obj){
+		if(obj){
+			setTimeout(function(){
+				obj.removeClass('ptfn-show').addClass('ptfn-hidden');
+				
+				pAlertCount--;
+				obj.remove();
+
+			}, 0);
+			console.log(pAlertCount);
+		}
+	}
+
+	console.log(pAlertCount);
 }
 //--------prettyfication-setup--------
